a50bd3ab883faa9ea8e232378a60e6adaacea503
deepakc <deepak.c@servify.in>
2020-03-03 15:40:18 +0530

    Transpile code using babel

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // const App = require('./app')
 
 class TestinApp {
-  renderEle = () => {
+  renderEle () {
     const root = document.createElement('div')
     root.innerHTML = 'Content elements entry point ok'
     document.body.appendChild(root)
